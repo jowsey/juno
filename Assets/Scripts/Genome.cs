74b3949cd60091dfd7c08cab3e0f21f48b08b449
Thomas Jowsey <2302820@uad.ac.uk>
2025-11-15 12:38:50 +0000

    More environment balancing

--- Assets/Scripts/Genome.cs
+++ Assets/Scripts/Genome.cs
@@ -1,37 +1,24 @@
-// using UnityEngine;
-//
-// public class Genome
-// {
-//         
-//     // Create a new random genome
-//     public Genome(int snapshotCount)
-//     {
-//         Snapshots = new ControlSnapshot[snapshotCount];
-//         for (var i = 0; i < snapshotCount; i++)
-//         {
-//             var snapshot = new ControlSnapshot
-//             {
-//                 ThrustControl = Random.Range(0f, 1f),
-//                 SteeringControl = Random.Range(-1f, 1f),
-//                 SeparateStage = Random.Range(0f, 1f) > 0.5f
-//             };
-//
-//             Snapshots[i] = snapshot;
-//         }
-//     }
-//
-//     // Combine two genomes to create a new genome
-//     public Genome(Genome parent1, Genome parent2)
-//     {
-//         var snapshotCount = parent1.Snapshots.Length;
-//         Snapshots = new ControlSnapshot[snapshotCount];
-//
-//         for (var i = 0; i < snapshotCount; i++)
-//         {
-//             var chosenParent = Random.value < 0.5f ? parent1 : parent2;
-//             var snapshot = chosenParent.Snapshots[i];
-//             Snapshots[i] = snapshot;
-//         }
-//     }
-// }
+using UnityEngine;
 
+public class Genome
+{
+    [Range(0f, 1f)] public float BoosterSeparationDelay;
+    [Range(0f, 1f)] public float MainStageSeparationDelay;
+
+    [Range(0f, 1f)] public float MaxQStartTime;
+    [Range(0f, 1f)] public float MaxQDuration;
+    [Range(0f, 1f)] public float MaxQThrustControl;
+    
+    [Range(0f, 1f)] public float GravityTurnStartTime;
+    [Range(0f, 1f)] public float GravityTurnDuration;
+
+    // Create a new random genome
+    public Genome(int snapshotCount)
+    {
+    }
+
+    // Combine two genomes to create a new genome
+    public Genome(Genome parent1, Genome parent2)
+    {
+    }
+}
