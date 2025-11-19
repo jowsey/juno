016e1c6dcb2c6f50549e0c69cedda203e6d54666
Thomas Jowsey <36897817+jowsey@users.noreply.github.com>
2025-11-19 14:46:42 +0000

    Improve camera zoom feel

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,11 +8,15 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _zoomRelativeMoveSpeed = 2f;
     [SerializeField] private float _minZoom = 5;
     [SerializeField] private float _maxZoom = 2000;
+    [SerializeField] private float _zoomSpeedLog = 1.2f;
+    [SerializeField] private float _baseZoomSpeed = 0.5f;
 
     public Transform FollowTarget;
 
     private InputAction _moveAction;
     private InputAction _zoomAction;
+
+    private float _targetZoom;
 
     private void OnValidate()
     {
@@ -23,6 +27,8 @@ public class CameraController : MonoBehaviour
     {
         _moveAction = InputSystem.actions.FindAction("Move");
         _zoomAction = InputSystem.actions.FindAction("Zoom");
+
+        _targetZoom = _camera.orthographicSize;
     }
 
     private void LateUpdate()
@@ -43,9 +49,10 @@ public class CameraController : MonoBehaviour
             _camera.transform.position += new Vector3(move.x, move.y, 0) * (moveSpeed * Time.unscaledDeltaTime);
         }
 
-        var zoom = _zoomAction.ReadValue<float>();
-        var logMultiplier = Mathf.Log(_camera.orthographicSize, 2f);
-        zoom *= logMultiplier;
-        _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize - zoom, _minZoom, _maxZoom);
+        var logMultiplier = Mathf.Log(_camera.orthographicSize, _zoomSpeedLog);
+        var zoom = _zoomAction.ReadValue<float>() * _baseZoomSpeed * logMultiplier;
+        _targetZoom = Mathf.Clamp(_targetZoom - zoom, _minZoom, _maxZoom);
+
+        _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _targetZoom, 10f * Time.unscaledDeltaTime);
     }
 }
